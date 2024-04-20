133569467e47c61f309db76ecc7d88aab520a94f
Ekramul Haque <ehr.ekramul@gmail.com>
2024-04-20 14:09:25 +0600

    Data Class

--- Basics/app/src/main/java/ehr/multiverse/kotlinbasics/Basics.kt
+++ Basics/app/src/main/java/ehr/multiverse/kotlinbasics/Basics.kt
@@ -1,12 +1,22 @@
 package ehr.multiverse.kotlinbasics
 
+data class CoffeeDetails(
+    val sugarCount:Int,
+    val name:String,
+    val size:String,
+    val creamAmount:Int
+)
 fun main(){
 
-    val daisy = Dog("Daisy")
-    println("Daisy Breed is ${daisy.breed}\n")
+    val coffeeForDenis = CoffeeDetails(0, "Denis", "xxl",0)
+    makeCoffee(coffeeForDenis)
 
-    daisy.age=12
-    println("Daisy age is ${daisy.age}\n")
+//    val daisy = Dog("Daisy")
+//    println("Daisy Breed is ${daisy.breed}\n")
+//
+//    daisy.age=12
+//    println("Daisy age is ${daisy.age}\n")
+
 
 //    val myBook = Book()
 //    println("Title of Book : ${myBook.title}")
@@ -18,10 +28,18 @@ fun main(){
 //    println("Author of Book : ${customBook.author}")
 //    println("Published Year of Book : ${customBook.yearPublished}\n")
 }
-fun add(num1: Int, num2: Int): Int {
-    return num1 + num2
-}
 
-fun divide(num1 : Int, num2 : Int) : Double{
-    return num1.toDouble()/num2
+fun makeCoffee(coffeeDetails: CoffeeDetails){
+    if(coffeeDetails.sugarCount==1)
+    {
+        println("Coffee With ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name}")
+    }
+    else if(coffeeDetails.sugarCount==0)
+    {
+        println("Coffee with no sugar for ${coffeeDetails.name}")
+    }
+    else
+    {
+        println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for${coffeeDetails.name}")
+    }
 }
