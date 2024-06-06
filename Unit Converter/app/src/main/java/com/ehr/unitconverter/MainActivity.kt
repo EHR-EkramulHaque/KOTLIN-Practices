8edc6d9983082adaf9206f496de188e9887d8deb
EHR-EkramulHaque <ehr.ekramul@gmail.com>
2024-06-06 20:13:14 +0600

    Day-5

--- Unit Converter/app/src/main/java/com/ehr/unitconverter/MainActivity.kt
+++ Unit Converter/app/src/main/java/com/ehr/unitconverter/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.ehr.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ehr.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,22 +51,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(){
-    Column{
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         //Here all the UI element will be stacked below each other
-        Text("Unit Converter")
 
+
+        Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
             //Here all the UI element will be stacked next to each other
 
+            Box(){//Left Select Button
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Millimeters") }, onClick = { /*TODO*/ })
+                }
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Box(){//Right Select Button
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Millimeters") }, onClick = { /*TODO*/ })
+                }
+            }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Result: ")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-
-    }
+fun UnitConverterPreview(){
+    UnitConverter()
 }
