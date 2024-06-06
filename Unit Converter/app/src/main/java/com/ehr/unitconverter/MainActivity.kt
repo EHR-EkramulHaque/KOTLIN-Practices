e540b7f28f7dba4326211943d4a88f1e3355a3fc
EHR-EkramulHaque <ehr.ekramul@gmail.com>
2024-06-06 18:23:35 +0600

    Starting

---
+++ Unit Converter/app/src/main/java/com/ehr/unitconverter/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.ehr.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ehr.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnitConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UnitConverter()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun UnitConverter(){
+    Column{
+        //Here all the UI element will be stacked below each other
+        Text("Unit Converter")
+
+
+        Row {
+            //Here all the UI element will be stacked next to each other
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UnitConverterTheme {
+
+    }
+}
